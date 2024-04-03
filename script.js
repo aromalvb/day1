2dc39997827e2cdc7c95d4d09b6de8807fa55361
aromalvb <165656826+aromalvb@users.noreply.github.com>
2024-04-03 15:17:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hello")
+
+// var,let,const
+
+var name="Aromal"
+console.log(name)
+
+var a=5
+var b=10
+if(a>b){
+    console.log("a is greater")
+}
+else{
+    console.log("b is greater")
+}
